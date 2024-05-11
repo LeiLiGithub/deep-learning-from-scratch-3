cc3f8a24766b9fabadd0a7bcee20bb3664f0c223
lileihlwrjyb <11089462@bbktel.com>
2024-05-11 15:03:31 +0800

    use local MNIST data

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -82,15 +82,21 @@ class MNIST(Dataset):
         super().__init__(train, transform, target_transform)
 
     def prepare(self):
-        url = 'http://yann.lecun.com/exdb/mnist/'
+        # url = 'http://yann.lecun.com/exdb/mnist/'
+        
+        local_folder = os.path.join(os.path.dirname(__file__), '../mnist_dataset')
+
         train_files = {'target': 'train-images-idx3-ubyte.gz',
                        'label': 'train-labels-idx1-ubyte.gz'}
         test_files = {'target': 't10k-images-idx3-ubyte.gz',
                       'label': 't10k-labels-idx1-ubyte.gz'}
 
         files = train_files if self.train else test_files
-        data_path = get_file(url + files['target'])
-        label_path = get_file(url + files['label'])
+        # data_path = get_file(url + files['target'])
+        # label_path = get_file(url + files['label'])
+
+        data_path = os.path.join(local_folder, files['target'])
+        label_path = os.path.join(local_folder, files['label'])
 
         self.data = self._load_data(data_path)
         self.label = self._load_label(label_path)
